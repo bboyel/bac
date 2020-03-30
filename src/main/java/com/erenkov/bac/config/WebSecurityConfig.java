1ebf900a3937b5e7beb6f14b0ae75e40404da14e
AleksandrErenkov <bboyel@mail.ru>
2020-03-30 20:12:29 +0400

    Открыл всем доступ к игре

--- src/main/java/com/erenkov/bac/config/WebSecurityConfig.java
+++ src/main/java/com/erenkov/bac/config/WebSecurityConfig.java
@@ -51,11 +51,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         // The pages does not require login
         http.authorizeRequests().antMatchers("/", "/login", "/logout", "/welcome", "/registration",     //
-                "logoutSuccessfulPage").permitAll();
+                "logoutSuccessfulPage","/gamepage").permitAll();
 
         // /userInfo page requires login as ROLE_USER or ROLE_ADMIN.
         // If no login, it will redirect to /login page.
-        http.authorizeRequests().antMatchers("/userInfo","/gamepage").access("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')");
+        http.authorizeRequests().antMatchers("/userInfo").access("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')");
 
         // For ADMIN only.
         http.authorizeRequests().antMatchers("/admin").access("hasRole('ROLE_ADMIN')");
